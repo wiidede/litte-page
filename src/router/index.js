b0a02a4c21ed991cbafee6a9252bb8c663413ad9
wiidede <wiixdede@gmail.com>
2021-11-11 13:52:48 +0800

    fix path error

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import {createRouter, createWebHashHistory} from 'vue-router';
 const routes = [
   {path: '/', name: 'Home',  component: () => import('/@/views/Home/index.vue')},
   {path: '/settings', name: 'Settings',  component: () => import('/@/views/Settings/index.vue')},
-  {path: '/settings/about', name: 'SettingsAbout',  component: () => import('/@/views/Settings/about.vue')},
+  {path: '/settings/about', name: 'SettingsAbout',  component: () => import('/@/views/Settings/About.vue')},
 ];
 
 export default createRouter({
