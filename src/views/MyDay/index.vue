714b1e6f59c393dede5de1650fdc28b620ab9fac
wiidede <wiixdede@gmail.com>
2021-11-19 00:41:09 +0800

    init myDay

---
+++ src/views/MyDay/index.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {reactive} from 'vue';
+const oneMinute = 60;
+const oneHour = 60 * oneMinute;
+const oneDay = 24 * oneHour;
+const timePoints = reactive([30 * oneMinute, oneHour]);
+const eventsName = reactive(['起床', null]);
+</script>
+
+<template>
+  <div>
+    <h1>{{ eventsName }}</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
