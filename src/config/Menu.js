b086bb8ec2f5c6958e87d142789fe15b5659d110
wiidede <wiixdede@gmail.com>
2021-11-23 14:37:42 +0800

    add cal icon

--- src/config/Menu.js
+++ src/config/Menu.js
@@ -1,7 +1,7 @@
 const menu = [
 	{name: 'Home', path: '/', active: false, icon: 'icon-home-line', iconActive: 'icon-home-fill'},
 	{name: 'Invest', path: '/invest-distribution', active: false, icon: 'icon-coins-line', iconActive: 'icon-coins-fill'},
-	{name: 'My Day', path: '/my-day', active: false, icon: 'icon-coins-line', iconActive: 'icon-coins-fill'},
+	{name: 'My Day', path: '/my-day', active: false, icon: 'icon-calendar-event-line', iconActive: 'icon-calendar-event-fill'},
 	{name: 'Settings', path: '/settings', active: false, icon: 'icon-settings-line', iconActive: 'icon-settings-fill'},
 ];
 
