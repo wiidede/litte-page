801b32700c8a15d0599845d676fc59e774dcdfff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-11-24 05:24:55 +0000

    Deploy wiidede/little-page to github.com/wiidede/little-page.git:gh-pages

---
+++ assets/index.109eee8e.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./index.017fccbf.js";import{o as s,f as _,g as a,t as n}from"./vendor.0dd81658.js";const o={name:"UserHome"};function c(d){const e="wiidede website";return(i,l)=>(s(),_("div",null,[a("h1",null,n(e))]))}const r=Object.assign(o,{setup:c});var f=t(r,[["__scopeId","data-v-1966aa29"]]);export{f as default};
+//# sourceMappingURL=index.109eee8e.js.map
--- assets/index.62519434.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./index.4adecc54.js";import{o as a,f as s,g as _,t as n}from"./vendor.0dd81658.js";const o={name:"UserHome"};function c(d){const e="wiidede website";return(i,l)=>(a(),s("div",null,[_("h1",null,n(e))]))}const r=Object.assign(o,{setup:c});var f=t(r,[["__scopeId","data-v-1966aa29"]]);export{f as default};
-//# sourceMappingURL=index.62519434.js.map
