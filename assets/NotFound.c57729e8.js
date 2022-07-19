a9867908a3323b2db6c7048d23b8f385fb4b54aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-07-19 06:24:05 +0000

    Deploy wiidede/little-page to github.com/wiidede/little-page.git:gh-pages

--- assets/NotFound.c57729e8.js
+++
@@ -1,2 +0,0 @@
-import{_ as n,o as d,c as r,b as e,u as s,ac as _,Y as i,Z as u,L as p}from"./index.86c49744.js";const a=o=>(i("data-v-56826d26"),o=o(),u(),o),l={id:"not-found-view"},f=a(()=>e("h1",null,"404 Not Found!",-1)),v=a(()=>e("i",{class:"iconfont icon-arrow-left-s-line"},null,-1)),h=p(" Go back"),k=[v,h],N={__name:"NotFound",setup(o){return(m,t)=>(d(),r("div",l,[f,e("strong",{class:"go-back",onClick:t[0]||(t[0]=(...c)=>s(_)&&s(_)(...c))},k)]))}};var g=n(N,[["__scopeId","data-v-56826d26"]]);export{g as default};
-//# sourceMappingURL=NotFound.c57729e8.js.map
