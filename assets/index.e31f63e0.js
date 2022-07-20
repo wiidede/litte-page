0ecb3329c2232e2d01371b35078b47bbe818beab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-07-20 12:55:21 +0000

    Deploy wiidede/little-page to github.com/wiidede/little-page.git:gh-pages

---
+++ assets/index.e31f63e0.js
@@ -0,0 +1,2 @@
+import"./base.4451971d.js";import{E as a}from"./el-scrollbar.2681aed3.js";import{_ as o,o as _,c as s,a as r,w as c,b as n,t as l}from"./index.5a82cee8.js";const i={id:"view-home-page"},d={name:"UserHome"},p=Object.assign(d,{setup(m){const e="wiidede website";return(u,f)=>{const t=a;return _(),s("div",i,[r(t,null,{default:c(()=>[n("h1",null,l(e))]),_:1})])}}});var h=o(p,[["__scopeId","data-v-fbd0771a"]]);export{h as default};
+//# sourceMappingURL=index.e31f63e0.js.map
