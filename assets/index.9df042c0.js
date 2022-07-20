4e55e7cd0749446bfdb17f69c1f0f3a504e7b711
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-07-20 02:57:48 +0000

    Deploy wiidede/little-page to github.com/wiidede/little-page.git:gh-pages

---
+++ assets/index.9df042c0.js
@@ -0,0 +1,2 @@
+import"./base.7c144dbd.js";import{E as a}from"./el-scrollbar.d8e19a36.js";import{_ as o,o as _,c as s,a as r,w as c,b as n,t as l}from"./index.348e64fd.js";const i={id:"view-home-page"},d={name:"UserHome"},p=Object.assign(d,{setup(m){const e="wiidede website";return(u,f)=>{const t=a;return _(),s("div",i,[r(t,null,{default:c(()=>[n("h1",null,l(e))]),_:1})])}}});var h=o(p,[["__scopeId","data-v-fbd0771a"]]);export{h as default};
+//# sourceMappingURL=index.9df042c0.js.map
--- assets/index.e46afe84.js
+++
@@ -1,2 +0,0 @@
-import"./base.b1f610a5.js";import{E as a}from"./el-scrollbar.e093fee0.js";import{_ as o,o as _,c as s,a as r,w as c,b as n,t as l}from"./index.e725c3eb.js";const i={id:"view-home-page"},d={name:"UserHome"},p=Object.assign(d,{setup(m){const e="wiidede website";return(u,f)=>{const t=a;return _(),s("div",i,[r(t,null,{default:c(()=>[n("h1",null,l(e))]),_:1})])}}});var h=o(p,[["__scopeId","data-v-fbd0771a"]]);export{h as default};
-//# sourceMappingURL=index.e46afe84.js.map
