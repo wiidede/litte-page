5d748411d58518c5a371fba10a4197ac9609f8e2
wiidede <wiixdede@gmail.com>
2022-07-29 23:08:46 +0800

    update resume;

--- src/views/MyResume/index.vue
+++ src/views/MyResume/index.vue
@@ -5,8 +5,8 @@ export default {
 </script>
 
 <script setup>
-import {useStore} from 'vuex';
-import {computed, onBeforeUnmount, ref} from 'vue';
+import { useStore } from 'vuex';
+import { computed, onBeforeUnmount, ref } from 'vue';
 
 const store = useStore();
 
@@ -37,13 +37,13 @@ onBeforeUnmount(() => {
 
 // content
 const skillTitle = [
-  {key: 'proficient', label: '熟悉'},
-  {key: 'familiar', label: '熟练使用'},
-  {key: 'know', label: '比较熟悉'},
+  { key: 'proficient', label: '熟悉' },
+  { key: 'familiar', label: '熟练使用' },
+  { key: 'know', label: '比较熟悉' },
 ];
 
 const skills = {
-  proficient: ['JS', 'CSS', 'Html','Vue', 'Vuex', 'Vue-router'],
+  proficient: ['JS', 'CSS', 'Html', 'Vue', 'Vuex', 'Vue-router'],
   familiar: ['ElementUI', 'Echarts', 'Git'],
   know: ['TypeScript', 'React', 'Webpack', 'Vite', 'Linux', 'Python', 'TensorFlow'],
 };
@@ -77,7 +77,9 @@ const awards = [
                 href="https://wiidede.github.io"
                 target="_blank"
                 style="margin-right: 12px"
-              ><i class="iconfont icon-blog-solid" />wiidede.github.io</a>
+              ><i
+                class="iconfont icon-blog-solid"
+              />wiidede.github.io</a>
               <a
                 href="https://github.com/wiidede"
                 target="_blank"
@@ -96,21 +98,6 @@ const awards = [
           </div>
         </div>
         <h2 class="title">
-          专业技能
-        </h2>
-        <div
-          v-for="cate in skillTitle"
-          :key="cate.key"
-        >
-          {{ cate.label }}
-          <template
-            v-for="(item, index) in skills[cate.key]"
-            :key="`proficient-${index}`"
-          >
-            {{ index ? '、' : '' }}<pre>{{ item }}</pre>
-          </template>
-        </div>
-        <h2 class="title">
           教育/工作经历
         </h2>
         <div>2017年09月 - 2021年06月 {{ sn('常熟') }}理工学院 计算机科学与工程学院 物联网工程 本科</div>
@@ -124,43 +111,41 @@ const awards = [
           苏州{{ sn('科达') }}是一家摄像头制造企业，其项目的主要业务都是设备的维护和为客户提供城市管理等解决方案。智能运维就是一套设备、用户、配置的管理及数据、图表动态展示的系统，方便客户更轻松的管理设备。
         </div>
         <ol>
-          <li>
-            配合公司前端框架修改项目框架：动态权限变化、三方登录、baseUrl支持相对路径
-          </li>
-          <li>
-            响应式echarts、el-tooltip封装溢出判断、前端导出excel、uKey登录
-          </li>
-          <li>
-            页面性能优化、UI细节优化、组件代码重构、减少es5、去除JQuery、更换三方库
-          </li>
-          <li>
-            独自开发子项目迷你巡检系统：不走公司统一登录、统一资源流程，合理覆写框架
-          </li>
-          <li>
-            开发子项目：工单管理（动态表格、动态表单）；自定义可视化（组件动态展示）；数据治理平台（数据展示、权限控制）等
-          </li>
+          <li>配合公司前端框架修改项目框架：动态权限变化；三方登录；baseUrl支持相对路径</li>
+          <li>响应式echarts组建；el-tooltip封装文字溢出判断；带有鉴权头的图片；年月日切换组件</li>
+          <li>前端导出excel；uKey登录；webSocket接受告警、进度、权限推送</li>
+          <li>页面性能优化；UI细节优化；组件代码重构；减少es5；去除JQuery；更换三方库</li>
+          <li>独自开发子项目迷你巡检系统：不走公司统一登录；统一资源流程，合理覆写框架</li>
+          <li>开发子项目：工单管理（动态表格；动态表单）；自定义可视化（组件动态展示）；数据治理平台（大量echarts图表展示、权限控制）；云平台运维（vis拓扑图）等；</li>
+          <li>支持部门其他项目的业务开发，如：解析平台、分布式存储系统、狮山大屏等项目</li>
         </ol>
         <strong class="project-title">运维中台项目（React + TypeScript + antd + Redux）</strong>
         <ol>
-          <li>
-            基于现成的低代码平台改造开发，修改100+缺陷。
-          </li>
-        </ol>
-        <strong class="project-title">云平台运维、解析平台、分布式存储系统、狮山大屏等项目（Vue + Element UI）</strong>
-        <ol>
-          <li>
-            项目支持、业务开发。
-          </li>
+          <li>该项目基于现成的低代码平台改造开发</li>
+          <li>全局日期组件添加格式；在antd3.x中新建YearPicker</li>
+          <li>不同组件间数据联动；下钻的逻辑优化；支持不同维度的属性各自与整体排序</li>
         </ol>
         <strong class="project-title">家庭服务机器人、水表识别（大学期间）</strong>
         <ol>
-          <li>
-            使用ROS系统操作机器人（C++），过程中涉及SLAM、建图、导航、路径规划等。
-          </li>
-          <li>
-            使用深度学习进行物品识别、从而提供家庭服务。顺带做了水表识别的项目。
-          </li>
+          <li>使用ROS系统操作机器人（C++），过程中涉及SLAM、建图、导航、路径规划等。</li>
+          <li>使用深度学习进行物品识别，从而提供家庭服务。顺带做了水表识别的项目。</li>
         </ol>
+        <h2 class="title">
+          专业技能
+        </h2>
+        <div
+          v-for="cate in skillTitle"
+          :key="cate.key"
+        >
+          {{ cate.label }}
+          <template
+            v-for="(item, index) in skills[cate.key]"
+            :key="`proficient-${index}`"
+          >
+            {{ index ? '、' : '' }}
+            <pre>{{ item }}</pre>
+          </template>
+        </div>
         <h2 class="title">
           获奖情况
         </h2>
@@ -178,7 +163,10 @@ const awards = [
           个人特质
         </h2>
         <div>重视团队协作。</div>
-        <div>对<pre>Vue</pre>源码有一定的理解。</div>
+        <div>
+          对
+          <pre>Vue</pre>源码有一定的理解。
+        </div>
         <div>励志写出逻辑完整、简洁的代码；写出性能良好、设计优美的前端页面。</div>
         <div>背单词；跑步；独立审美；热爱科技；摄影；追求高效与简洁的生活方式。</div>
         <a
@@ -251,7 +239,8 @@ pre {
   gap: 6px;
 }
 
-h1, .title {
+h1,
+.title {
   color: var(--main)
 }
 
